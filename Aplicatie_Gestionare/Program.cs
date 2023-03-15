6cc1af4e8eb7628ef7de1a01fa6180620808e3e7
user <user@DESKTOP-D6M2CLN>
2023-03-15 11:50:05 +0200

    Add project files.

---
+++ Aplicatie_Gestionare/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicatie_Gestionare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Produs
+    {
+        // format fizic, digital + merchandise
+        void Valabilitate()
+        { }
+    }
+
+    class Magazin
+    {
+
+    }
+
+    class Format
+    {
+        // formate disponibile (cd, vinyl, digital download)
+    }
+
+    class Catalog
+    {
+        //constructori de initializare
+    }
+
+
+    class Promotie
+    {
+        // perioade de promotii
+    }
+}
+
+class Pret
+{ 
+    
+}
